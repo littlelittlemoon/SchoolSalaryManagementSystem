4cb4b57ebbf49caf28351204f935abdc45106fc0
Luo Liang <ll9420416@outlook.com>
2017-04-16 20:40:06 +0800

    添加跨域请求过滤
    Signed-off-by: Luo Liang <ll9420416@outlook.com>

---
+++ src/main/java/org/ssms/interceptor/SimpleCORSFilter.java
@@ -0,0 +1,34 @@
+package org.gp.core.interceptors;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by luoliang on 2017/2/6.
+ * 跨域请求过滤器
+ */
+@Component
+public class SimpleCORSFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
